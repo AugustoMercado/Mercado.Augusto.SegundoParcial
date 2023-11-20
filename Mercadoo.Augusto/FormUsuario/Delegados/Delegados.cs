bd0530cb7c5716e6a4f658807571aeaa4b1ec2a2
Augusto Mercado <Joaquinaugusto7@gmail.com>
2023-11-20 13:50:06 -0300

    agregar delegados a la clase

--- Mercadoo.Augusto/FormUsuario/Delegados/Delegados.cs
+++ Mercadoo.Augusto/FormUsuario/Delegados/Delegados.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using PrimerParcial;
+
+/// <summary>
+/// Delegado para llamar a los metodos de las clases hijas.
+/// </summary>
+/// <param name="cadena">conexion a la base</param>
+/// <param name="lista">donde se van a guardar los personajes</param>
+/// <returns>retorna una lista con los personajes</returns>
+public delegate Ejercito Datos(string cadena, Ejercito lista);
+
+/// <summary>
+/// Delegado para llamar a Datos.
+/// </summary>
+public delegate void ObtenerDatos();
