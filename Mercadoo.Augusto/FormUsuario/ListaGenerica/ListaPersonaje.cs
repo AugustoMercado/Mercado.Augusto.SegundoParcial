4118f0c6e44be86bc122a7ffc87a7d19a4efb7df
Augusto Mercado <Joaquinaugusto7@gmail.com>
2023-11-21 00:54:35 -0300

    atributo + constructor

--- Mercadoo.Augusto/FormUsuario/ListaGenerica/ListaPersonaje.cs
+++ Mercadoo.Augusto/FormUsuario/ListaGenerica/ListaPersonaje.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PrimerParcial;
 
 namespace FormUsuario.ListaGenerica
 {
-    public class ListaPersonaje
+    public class ListaPersonaje<T> where T : Personaje
     {
+        private string cadenaConexion;
+
+        public ListaPersonaje(string stringConexion)
+        {
+            this.cadenaConexion = stringConexion;
+        }
+
     }
 }
