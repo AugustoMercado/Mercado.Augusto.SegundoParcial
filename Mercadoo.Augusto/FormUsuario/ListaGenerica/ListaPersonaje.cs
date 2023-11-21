f1c7e9b03ecd973eaf3577058e3847887d32cd43
Augusto Mercado <Joaquinaugusto7@gmail.com>
2023-11-21 01:06:48 -0300

    metodo agregado en la lista generica

--- Mercadoo.Augusto/FormUsuario/ListaGenerica/ListaPersonaje.cs
+++ Mercadoo.Augusto/FormUsuario/ListaGenerica/ListaPersonaje.cs
@@ -4,17 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PrimerParcial;
+using Microsoft.Data.SqlClient;
 
 namespace FormUsuario.ListaGenerica
 {
     public class ListaPersonaje<T> where T : Personaje
     {
+        #region Atributo
         private string cadenaConexion;
+        #endregion
 
+        #region Constructor
         public ListaPersonaje(string stringConexion)
         {
             this.cadenaConexion = stringConexion;
         }
 
+        #endregion
+
+
+        #region Metodo
+        public bool EliminarPersonaje(T personaje)
+        {
+            SqlConnection conexion = new(this.cadenaConexion);
+            bool retorno = false;
+
+            try
+            {
+                SqlCommand sqlComando = new SqlCommand();
+                sqlComando.CommandType = System.Data.CommandType.Text;
+                sqlComando.CommandText = $"DELETE FROM {typeof(T)} WHERE Id = @Id";
+                sqlComando.Parameters.AddWithValue("@id", personaje.id);
+
+                sqlComando.Connection = conexion;
+                conexion.Open();
+
+                int filasAfectadas = sqlComando.ExecuteNonQuery();
+
+                if (filasAfectadas == 1)
+                {
+                    retorno = true;
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error al eliminar el arquero: " + e.Message);
+                retorno = false;
+            }
+
+            return retorno;
+        }
+        #endregion
     }
 }
