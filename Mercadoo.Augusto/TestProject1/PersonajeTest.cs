4f486afd891809e3f4ea2951c58096a397500d7d
Augusto Mercado <Joaquinaugusto7@gmail.com>
2023-12-06 01:02:41 -0300

    Metodo Testing Guerrero

--- Mercadoo.Augusto/TestProject1/PersonajeTest.cs
+++ Mercadoo.Augusto/TestProject1/PersonajeTest.cs
@@ -7,9 +7,32 @@ namespace TestProject1
     public class PersonajeTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void VerificarGuerrerosIguales_true()
         {
-    
+            // Arrange
+            Guerrero g1 = new Guerrero(10, 10, 50, "Guerrero1");
+            Guerrero g2 = new Guerrero(40, 20, 10, "Guerrero1");
+
+            // Act
+            bool resultado = g1.Equals(g2);
+
+            // Assert
+            Assert.IsTrue(resultado);
+
+        }
+        [TestMethod]
+        public void VerificarGuerrerosIguales_false()
+        {
+            // Arrange
+            Guerrero g1 = new Guerrero(10, 10, 50, "Guerrero1");
+            Guerrero g2 = new Guerrero(40, 20, 10, "Guerrero2");
+
+            // Act
+            bool resultado = g1.Equals(g2);
+
+            // Assert
+            Assert.IsFalse(resultado);
+
         }
     }
 }
